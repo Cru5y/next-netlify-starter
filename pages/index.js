1e7dbe61a1c40c5962519a963a0cf4b199c72e84
Cru5y <radhesyam@live.it>
2023-12-14 12:00:09 +0100

    testing ai calls 1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
+import OpenAI from 'openai';
+import React, { useState, useEffect } from 'react';
+
+//async function main() {
+//  const completion = await openai.chat.completions.create({
+//    model: 'gpt-4',
+//    messages: [{ role: 'user', content: 'Say this is a test' }],
+//  });
+//  console.log(completion.choices[0]?.message?.content);
+//}
 
 export default function Home() {
+
+  const [userInput, setUserInput] = useState('');
+  const [generatedText, setGeneratedText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const openai = new OpenAI({ apiKey: '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', dangerouslyAllowBrowser: true })
+
+  async function handleSubmit() {
+    setIsLoading(true);
+    try {
+      const completion = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: [{ role: 'user', content: userInput }],
+      });
+      setGeneratedText(completion.choices[0]?.message?.content);
+    } catch (error) {
+      console.error('Error:', error);
+      setGeneratedText('Error fetching response');
+    }
+    setIsLoading(false);
+  }
+
+  function handleInputChange(event) {
+    setUserInput(event.target.value);
+  }
+
   return (
     <div className="container">
       <Head>
@@ -15,6 +51,16 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <input
+        type="text"
+        value={userInput}
+        onChange={handleInputChange}
+        placeholder="Enter your prompt"
+      />
+      <button onClick={handleSubmit} disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Submit'}
+      </button>
+      <p>{generatedText}</p>
       </main>
 
       <Footer />
